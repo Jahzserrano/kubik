8e970f7015233624f821dd65e4491971d68f1c57
jahzserrano <serranojahaziel2@gmail.com>
2024-05-29 09:46:48 -0400

    fix NgForm import Problem

--- kubik/src/app/login/login.component.ts
+++ kubik/src/app/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
